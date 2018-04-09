0136011b82fa208c072cc9b55de4eb4ebe64c9ee
Tirta Mahardika <37982937+tirtamahardika@users.noreply.github.com>
2018-04-09 14:59:04 +0700

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,15 @@
+# cara-unggah-file-dan-tokenisasi
+beberapa langkah untuk upload file dan cara tokenisasi 
+
+<html>
+<title>Form Upload</title>
+<body>
+<form enctype="multipart/form-data" method="POST" action="hasil_upload.php">
+File yang di upload : <input type="file" name="fupload"><br>
+Deskripsi File : <br>
+<textarea name="deskripsi" rows="8" cols="40"></textarea><br>
+<input type=submit value=Upload>
+</form>
+
+//anjar saputra 15.01.55.0010
+//sistem informasi unisbank semarang
